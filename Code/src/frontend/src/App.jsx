7013aceebd698dd50a45e8aef157359ce1af297e
Geovanna Domingos <gmdomingosn@gmail.com>
2023-09-04 19:03:58 -0300

    frontend: adiciona navbar

--- Code/src/frontend/src/App.jsx
+++ Code/src/frontend/src/App.jsx
@@ -1,11 +1,45 @@
 
 import './App.css'
+import logo from './assets/colabora.png'
+import lupa from './assets/lupa.png'
+import mais from './assets/mais.png'
 
 function App() {
 
   return (
     <>
-      <h1>ColaboraCIn</h1>
+      <header>
+        <nav>
+          <div className='nav-div1'>
+            <h1 className='nav-h1'>ColaboraCIn</h1>
+            <img
+              src={logo}
+              alt="Logo"
+              className='nav-img'
+            />
+          </div>
+          <div className='nav-div2'>
+            <a className='nav-div-a' href="#">
+              <img
+                src={lupa}
+                alt="Icone de lupa"
+                className='nav-img2'
+              />
+            </a>
+            <div className='nav-div2-div'>
+              <a className='nav-div-a' href="#">
+                Adicionar material
+              </a>
+              <img
+                src={mais}
+                alt="Icone de mais"
+                className='nav-img2'
+              />
+            </div>
+          </div>
+        </nav>
+      </header>
+
     </>
   )
 }
