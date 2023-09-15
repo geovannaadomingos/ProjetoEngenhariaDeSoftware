bbdaf0deed408a4795da49621d0b7b9f1463724d
Geovanna Mayra Domingos do Nascimento <gmdomingosn@gmail.com>
2023-09-15 15:15:02 -0300

    frontend: ajuste no css da  tela de login

--- Code/src/frontend/src/pages/Login.jsx
+++ Code/src/frontend/src/pages/Login.jsx
@@ -1,31 +1,31 @@
-// import React, { useEffect } from 'react';
-// import jwt_decode from 'jwt-decode';
+import React, { useEffect } from 'react';
+import jwt_decode from 'jwt-decode';
 import './Login.css';
 import logo from '../assets/colabora.png';
 
 function Login(props) {
-    // const [user, setUser] = React.useState({});
+    const [user, setUser] = React.useState({});
 
-    // function handleCredentialResponse(response) {
-    //     console.log("Encoded JWT ID token: " + response.credential);
-    //     let userObject = jwt_decode(response.credential);
-    //     console.log(userObject);
-    //     setUser(userObject);
+    function handleCredentialResponse(response) {
+        console.log("Encoded JWT ID token: " + response.credential);
+        let userObject = jwt_decode(response.credential);
+        console.log(userObject);
+        setUser(userObject);
 
-    //     // Após a autenticação bem-sucedida, chame a função para definir isAuthenticated como true
-    //     props.setAuthenticated();
-    // }
+        // Após a autenticação bem-sucedida, chame a função para definir isAuthenticated como true
+        // props.setAuthenticated();
+    }
 
-    // useEffect(() => {
-    //     google.accounts.id.initialize({
-    //         client_id: '394323047185-va2rp8rso0bgor9s49o65rp4otul602r.apps.googleusercontent.com',
-    //         callback: handleCredentialResponse,
-    //     });
-    //     google.accounts.id.renderButton(
-    //         document.getElementById('signInDiv'),
-    //         { theme: 'outline', size: 'large' }
-    //     );
-    // }, []);
+    useEffect(() => {
+        google.accounts.id.initialize({
+            client_id: '394323047185-va2rp8rso0bgor9s49o65rp4otul602r.apps.googleusercontent.com',
+            callback: handleCredentialResponse,
+        });
+        google.accounts.id.renderButton(
+            document.getElementById('signInDiv'),
+            { theme: 'outline', size: 'large' }
+        );
+    }, []);
 
     return (
         <div className='div'>
@@ -38,19 +38,10 @@ function Login(props) {
                 />
             </div>
             <div className='div-button'>
+                <h2 className='div-button-h2'>Faça seu login com a conta de domínio cin.ufpe.br e tenha acesso aos materiais do ColaboraCIn:</h2>
                 <div id='signInDiv'></div>
             </div>
         </div>
-        // <div className='Login'>
-        //     <div id='signInDiv'></div>
-        //     {user &&
-        //         <div>
-        //             <img src={user.picture} alt="Profile" />
-        //             <h3>{user.name}</h3>
-        //             <p>{user.email}</p>
-        //             <h2>{user.email_verified ? 'Email verificado' : 'Email não verificado'}</h2>
-        //         </div>}
-        // </div>
     );
 }
 
