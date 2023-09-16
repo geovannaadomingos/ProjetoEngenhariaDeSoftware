c7580009e380bf3bc2bf4d306e6357575b571559
Geovanna Domingos <gmdomingosn@gmail.com>
2023-09-16 16:32:38 -0300

    frontend: adiciona link de download no card

--- Code/src/frontend/src/components/Card.jsx
+++ Code/src/frontend/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import "./Card.css";
+import download from "../assets/download.png";
 
 function Card(props) {
   const {
@@ -15,17 +16,27 @@ function Card(props) {
   return (
     <div className="card">
       <div className="rectangle" />
-      <div className="details">
-        <h4 className="details-h4">{titulo}</h4>
-        <p className="details-p">Professor(a): {professor}</p>
-        <p className="details-p">Autor(a): {autor}</p>
-        <p className="details-p">Assunto: {assunto}</p>
-        <p className="details-p">Curso: {curso}</p>
+      <div className="header">
+        <h4 className="header-h4">{titulo}</h4>
+        <p className="header-p">Professor(a): {professor}</p>
+        <p className="header-p">Autor(a): {autor}</p>
+        <p className="header-p">Assunto: {assunto}</p>
+        <p className="header-p">Curso: {curso}</p>
       </div>
-      <div className="footer">
-        <div className="footer-div">{semestre}</div>
-        <div className="footer-div">{periodo} período</div>
-        <div className="footer-div">{codigoDisciplina}</div>
+      <div className="main">
+        <div className="main-div">{semestre}</div>
+        <div className="main-div">{periodo} período</div>
+        <div className="main-div">{codigoDisciplina}</div>
+      </div>
+      <div className='footer'>
+        <a className='footer-a' href="#">
+          Fazer download
+          <img
+            src={download}
+            alt="Icone de download"
+            className='footer-img'
+          />
+        </a>
       </div>
     </div>
   );
