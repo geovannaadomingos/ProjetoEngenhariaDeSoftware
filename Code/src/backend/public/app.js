6e32831050162ee9296eaa4c6b419bf59e1d5c4e
Geovanna Mayra Domingos do Nascimento <gmdomingosn@gmail.com>
2023-09-20 15:35:38 -0300

    backend: confuração do CORS

--- Code/src/backend/public/app.js
+++ Code/src/backend/public/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Configurando o middleware para permitir CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(express.json());
 
 const routes = require('./rotas/routes');
